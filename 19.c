cd1043319b81d77a092c75009935094bcd120f8a
savvidis4 <charissavvidis4@gmail.com>
2024-02-09 16:25:32 +0200

    19

---
+++ 19.c
@@ -0,0 +1,29 @@
+/*Να γραφεί συνάρτηση η οποία θα γεμίζει έναν πίνακα χαρακτήρων με τυχαίες τιμές.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void fill(char* arr);
+
+int main(void){
+
+    char* arr = malloc(8);
+
+    fill(arr);
+
+    printf("%s", arr);
+}
+
+void fill(char* arr){
+
+    srand(time(NULL));
+
+    for (int i = 0; i < 8; i++){
+
+        int randnum = (rand() % (107)) + 20;
+
+        arr[i] = (char) randnum;
+    }
+    return;
+}
